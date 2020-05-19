91a70c410a107e8baf3914f62afec05d6f51046e
Kummer Martin <k.martin2330@gmail.com>
2020-05-19 08:41:01 +0200

    Added manual input for price slider, api call changed to arrow function, cleanup...

--- src/views/activity.vue
+++ src/views/activity.vue
@@ -5,7 +5,8 @@
 				<h3 class="mb-2">You Should</h3>
 				<v-textarea :loading="isLoading" autofocus dense solo v-model="activity.activity"></v-textarea>
 
-				<v-btn :small="$vuetify.breakpoint.mdAndDown" @click="saveActivity(activity)" class="red white--text" block><small>Save for later</small></v-btn>
+				<v-btn :small="$vuetify.breakpoint.mdAndDown" @click="saveActivity(activity)" class="red white--text" block>
+					<small>Save for later</small></v-btn>
 			</v-col>
 			<v-col cols="$vuetify.breakpoint.mdAndDown ? 12 : 6">
 				<h3 class="mb-2">Activity Details:</h3>
@@ -22,16 +23,15 @@
 						dense
 				></v-text-field>
 
-				<v-label for="budget">Budget</v-label>
+				<v-label for="budget">Budget - {{ budget }}</v-label>
 				<v-slider @change="getActivity()" id="budget" step="0.1" min="0" :max="maxPrice" ticks v-model="activity.price">
-					<template slot="prepend">
-						{{ activity.price }}
-					</template>
 					<template slot="append">
-						{{ budget }}
+						<v-text-field type="number" step="0.1" min="0" :max="maxPrice" class="mt-0 pt-0" single-line dense style="width: 60px" v-model="activity.price"></v-text-field>
 					</template>
 				</v-slider>
-				<v-btn :small="$vuetify.breakpoint.mdAndDown" :disabled="isLoading" :loading="isLoading" @click="getActivity(true)" class="blue white--text" block><small>Hit me with a new one!</small></v-btn>
+
+				<v-btn :small="$vuetify.breakpoint.mdAndDown" :disabled="isLoading" :loading="isLoading" @click="getActivity(false)" class="blue white--text" block>
+					<small>Hit me with a new one!</small></v-btn>
 			</v-col>
 		</v-row>
 		<v-snackbar v-model="error" top color="red">
@@ -57,60 +57,63 @@
                     participants: 1,
                     price: 0,
                 },
-                dataChangedRecently: false,
                 error: false,
                 errorMsg: '',
-		            isLoading: false,
-		            maxPrice: 100,
+                isLoading: true,
+                maxPrice: 10,
                 types: ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
             }
         },
         computed: {
             budget() {
-								if(this.activity.price == 0) return 'Free'
-								if(this.activity.price <= (this.maxPrice * 0.50)) return 'Cheap'
-								if(this.activity.price >= (this.maxPrice * 0.51)) return 'Expensive'
-
+                if (this.activity.price == 0) {
+                    return 'Free'
+                }
+                if (this.activity.price <= (this.maxPrice * 0.50)) {
+                    return 'Cheap'
+                }
+                if (this.activity.price >= (this.maxPrice * 0.51)) {
+                    return 'Expensive'
+                }
             },
             myActivities() {
                 return this.$store.state.myActivities
             }
         },
         mounted() {
-
             this.getActivity()
-
         },
         methods: {
-            getActivity(withoutParams) {
-                let x = this;
+            getActivity(withParams = true) {
+                this.isLoading = true;
                 let queryParams = '';
                 let tmp = {
                     type: this.activity.type,
                     participants: this.activity.participants,
                     price: this.activity.price,
                 }
-								if(!withoutParams){
-                  queryParams = `?` + new URLSearchParams(tmp).toString()
-								}
-		            this.isLoading = true;
-                axios.get(`http://www.boredapi.com/api/activity` + queryParams).then(function (response) {
-                    if (response.data) {
-                        if (!response.data.hasOwnProperty('error')) {
-                            x.setData(response.data);
-                            x.error = false
-                        } else {
-                            x.error = true
-                            x.errorMsg = response.data.error
-                        }
+
+                if (withParams) {
+                    queryParams = `?` + new URLSearchParams(tmp).toString()
+                }
+
+                axios.get(`http://www.boredapi.com/api/activity` + queryParams).then(response => {
+                    if (response.data && response.data.hasOwnProperty('error')) {
+                        this.error = true;
+                        this.errorMsg = response.data.error;
+                        return false;
                     }
-		                x.isLoading = false;
-                })
+
+                    this.setData(response.data);
+                    this.error = false;
+                }).finally(() => {
+                    this.isLoading = false;
+                });
             },
             setData(data) {
                 this.activity = data;
             },
-		        saveActivity() {
+            saveActivity() {
                 this.$store.dispatch('saveActivity', this.activity)
             }
         },
