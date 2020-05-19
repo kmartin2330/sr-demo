26115c46ef735c457579f653f602a414c6380708
Kummer Martin <k.martin2330@gmail.com>
2020-05-19 15:40:06 +0200

    fix list layout 'empty' notification

--- src/views/list.vue
+++ src/views/list.vue
@@ -1,36 +1,47 @@
 <template>
-	<div>
-		<v-simple-table v-if="myActivities.length">
-			<template v-slot:default>
-				<thead>
-				<tr>
-					<th>#</th>
-					<th>Activity</th>
-					<th>Participants</th>
-					<th>Budget</th>
-					<th>
-						<v-icon>mdi-pencil</v-icon>
-					</th>
-				</tr>
-				</thead>
-				<tbody>
-				<tr v-for="(activity,key) in myActivities" :key="key">
-					<td>{{ key }}</td>
-					<td>{{ activity.activity }}</td>
-					<td>{{ activity.participants }}</td>
-					<td>{{ activity.price }}</td>
-					<td>
-						<v-btn small icon color="red" title="Törlés" @click="removeActivityByKey(key)">
-							<v-icon>mdi-trash-can-outline</v-icon>
-						</v-btn>
-					</td>
-				</tr>
-				</tbody>
-			</template>
-		</v-simple-table>
-		<v-alert v-else class="ma-5" outlined width="100%" color="orange" dense>No saved Activities here.</v-alert>
-		<v-btn v-show="myActivities.length > 0" block color="red white--text" @click="clearMyActivities()">Clear All</v-btn>
-	</div>
+<v-container>
+		<v-row>
+			<v-col cols="12">
+				<v-simple-table class="mb-5" v-if="myActivities.length">
+					<template v-slot:default>
+						<thead>
+						<tr>
+							<th>#</th>
+							<th>Activity</th>
+							<th>Participants</th>
+							<th>Budget</th>
+							<th>
+								<v-icon>mdi-pencil</v-icon>
+							</th>
+						</tr>
+						</thead>
+						<tbody>
+						<tr v-for="(activity,key) in myActivities" :key="key">
+							<td>{{ key }}</td>
+							<td>{{ activity.activity }}</td>
+							<td>{{ activity.participants }}</td>
+							<td>{{ activity.price }}</td>
+							<td>
+								<v-btn small icon color="red" title="Törlés" @click="removeActivityByKey(key)">
+									<v-icon>mdi-trash-can-outline</v-icon>
+								</v-btn>
+							</td>
+						</tr>
+						</tbody>
+					</template>
+				</v-simple-table>
+
+				<v-container v-else>
+					<v-row>
+						<v-alert class="ma-5" outlined width="100%" color="orange" dense>No saved Activities here.</v-alert>
+					</v-row>
+				</v-container>
+
+				<v-btn v-show="myActivities.length > 0" block color="red white--text" @click="clearMyActivities()">Clear All</v-btn>
+			</v-col>
+		</v-row>
+</v-container>
+
 </template>
 
 <script>
